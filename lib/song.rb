7c092ba35bb51e48f532dff4d1216f43ee7f0568
Rebecca Congi <rncongi@gmail.com>
2020-08-23 23:51:49 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,24 @@
+class Song 
+  attr_accessor :artist, :name 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    save
+  end   
+  
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
+  end   
+  
+  def save 
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+end   
